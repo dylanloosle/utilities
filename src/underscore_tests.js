f1e5a4dfa3b83d50a0ab64273225f425b43c66fb
Dylan Loosle <dylanloosle@gmail.com>
2015-07-19 16:16:37 -0600

    one more

--- src/underscore_tests.js
+++ src/underscore_tests.js
@@ -42,11 +42,19 @@ var _ = { };
   // Call iterator(value, key, collection) for each element of collection.
   // Accepts both arrays and objects.
   _.each = function(collection, iterator) {
+
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
   // is not present in the array.
   _.indexOf = function(array, target){
+    if(!array.indexOf(target)){
+      return -1;
+    }
+    else{
+      return array.indexOf(target);
+    }
+
   };
 
   // Return all elements of an array that pass a truth test.
